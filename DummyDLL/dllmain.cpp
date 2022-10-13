70c972da6f3a4c7ed57e5923d97b23b24f6b01fb
j5 <j5>
2022-10-13 13:50:08 -0700

    v2

--- DummyDLL/dllmain.cpp
+++ DummyDLL/dllmain.cpp
@@ -3,17 +3,12 @@
 //===============================================================================================//
 #include "ReflectiveLoader.h"
 #include "strsafe.h"
-
+#include <stdlib.h>
+#include <string>
+#include <fstream>
 // Note: REFLECTIVEDLLINJECTION_VIA_LOADREMOTELIBRARYR and REFLECTIVEDLLINJECTION_CUSTOM_DLLMAIN are
 // defined in the project properties (Properties->C++->Preprocessor) so as we can specify our own 
 // DllMain and use the LoadRemoteLibraryR() API to inject this DLL.
-
-extern "C" __declspec(dllexport) DWORD CallFunc(char *lpParam)
-{
-	MessageBoxA(NULL, lpParam, "Reflective Dll Injection", MB_OK | MB_SYSTEMMODAL);
-	return 1;
-}
-
 // You can use this value as a pseudo hinstDLL value (defined and set via ReflectiveLoader.c)
 extern "C" HINSTANCE hAppInstance;
 
@@ -37,11 +32,17 @@ BOOL IsElevated() {
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved)
 {
 	DWORD pid = 0;
-	TCHAR path[100];
-	TCHAR user[100];
+	TCHAR path[MAX_PATH];
+	TCHAR user[MAX_PATH];
+	TCHAR context[MAX_PATH];
 	DWORD size_user = sizeof(user);
 	TCHAR msg[400];
 	BOOL bReturnValue = TRUE;
+	std::wstring logpath;
+	std::wofstream outfile;
+	wchar_t* publicfolder;
+	LPCWSTR Path;
+
 	switch (dwReason)
 	{
 	case DLL_QUERY_HMODULE:
@@ -50,21 +51,34 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved)
 		break;
 	case DLL_PROCESS_ATTACH:
 		hAppInstance = hinstDLL;
-		pid = GetCurrentProcessId();
-		GetModuleFileName(NULL, path, 1000);
-		GetUserName(user, &size_user);
-		StringCbPrintf(msg, 400, TEXT("%s %u %s %s %s %s"), TEXT("PID:"), pid, TEXT("\nProcess:"), path, TEXT("\nUser: "), user);
 
+		// Public folder (C:\\Users\\Public)
+		publicfolder = _wgetenv(L"PUBLIC");
+		logpath = publicfolder;
+		logpath += std::wstring(L"\\dummy.log");
+		Path = logpath.c_str();
+
+		pid = GetCurrentProcessId();
+		GetModuleFileName(NULL, path, MAX_PATH);
+		GetUserName(user, &size_user);
 		BOOL isAdmin;
 		isAdmin = IsElevated();
 		if (isAdmin) {
-			MessageBox(NULL, msg, TEXT("Elevated*"), MB_OK | MB_SYSTEMMODAL);
-			break;
+			wcscpy(context, TEXT("Elevated*"));
 		}
 		else {
-			MessageBox(NULL, msg, TEXT("User"), MB_OK | MB_SYSTEMMODAL);
-			break;
+			wcscpy(context, TEXT("User"));
 		}
+
+		// Write log %PUBLIC%\dummy.log
+		outfile.open(Path, std::ios_base::app);
+		outfile << TEXT("PID:") << pid << std::endl;
+		outfile << TEXT("Process:") << path << std::endl;
+		outfile << TEXT("User:") << user << std::endl;
+		outfile << TEXT("Context:") << context << std::endl << std::endl;
+		outfile.close();
+
+
 	case DLL_PROCESS_DETACH:
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
